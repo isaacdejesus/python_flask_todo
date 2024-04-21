972496a5e5d3a9ff0c3451e9ee6953254c1072ca
isaac <idreyes@uh.edu>
2024-04-21 11:23:38 -0500

    new repo with git ignores

---
+++ app.py
@@ -0,0 +1,62 @@
+from flask import Flask, render_template, redirect, request
+from flask_scss import Scss
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+Scss(app)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+db = SQLAlchemy(app)
+class MyTask(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(100), nullable=False)
+    complete = db.Column(db.Integer, default=0)
+    created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __rep__(self)-> str:
+        return f"Task {self.id}"
+
+@app.route("/", methods=["POST", "GET"])
+def index():
+    if request.method == "POST":
+        current_task = request.form['content']
+        new_task = MyTask(content=current_task)
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect("/")
+        except Exception as e:
+            print(f"ERROR:{e}")
+            return F"ERROR:{e}"
+    else:
+        tasks = MyTask.query.order_by(MyTask.created).all()
+        return render_template('index.html', tasks=tasks)
+#delete
+@app.route("/delete/<int:id>")
+def delete(id:int):
+    delete_task = MyTask.query.get_or_404(id)
+    try:
+        db.session.delete(delete_task)
+        db.session.commit()
+        return redirect("/")
+    except Exception as e:
+        return F"ERROR:{e}"
+#edit
+@app.route("/edit/<int:id>", methods=["GET", "POST"])
+def edit(id:int):
+    task = MyTask.query.get_or_404(id)
+    if request.method == "POST":
+        task.content = request.form['content']
+        try:
+            db.session.commit()
+            return redirect("/")
+        except Exception as e:
+            return f"Error:{e}"
+    else:
+        return render_template('edit.html', task=task)
+
+if __name__ in "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
